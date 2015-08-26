81c1a853c8100dd8f99b69643e159217a1a783e8
Fumiaki Kinoshita <fumiexcel@gmail.com>
2015-08-26 17:22:10 +0900

    Another way to compose

--- src/Data/Boombox.hs
+++ src/Data/Boombox.hs
@@ -1,5 +1,6 @@
 module Data.Boombox (driveTape
   , (@->)
+  , (-@>)
   , (>-$)
   , module Data.Boombox.Tape
   , module Data.Boombox.Drive) where
@@ -18,14 +19,24 @@ driveTape t (Eff m) = m >>= driveTape t
 driveTape (Effect m) d = m >>= (`driveTape` d)
 driveTape (Yield a wcont) (Partial f) = driveTape (extract wcont) (f a)
 
-(@->) :: (Comonad v, Functor w, Functor m) => Tape v m a -> Tape w (Drive Void a m) b -> Tape w m b
-y@(Yield a vcont) @-> Effect d = case d of
-  Partial f -> extract vcont @-> Effect (f a)
-  Done s k -> y @-> commitTape (supplyDrive s) k
-  Eff m -> Effect $ fmap ((y @->) . Effect) m
+(-@>) :: (Comonad v, Functor w, Functor m) => Tape v m a -> Tape w (Drive Void a m) b -> Tape w m b
+y@(Yield a vcont) -@> Effect d = case d of
+  Partial f -> extract vcont -@> Effect (f a)
+  Done s k -> y -@> commitTape (supplyDrive s) k
+  Eff m -> Effect $ fmap ((y -@>) . Effect) m
   Failed _ v -> absurd v
-t @-> Yield b w = Yield b $ fmap (t @->) w
-Effect m @-> t = Effect $ fmap (@->t) m
+t -@> Yield b w = Yield b $ fmap (t -@>) w
+Effect m -@> t = Effect $ fmap (-@>t) m
+
+(@->) :: (Comonad w, Comonad v, Monad m) => Tape w m a -> Tape v (Drive Void a m) b -> Tape w m b
+Yield a w @-> rec = Effect $ fmap extract $ go rec where
+  go (Yield b cont) = extend (Yield b) <$> go (extract cont)
+  go (Effect d) = case d of
+    Done s k -> return $ fmap (@->commitTape (supplyDrive s) k) w
+    Partial f -> return $ fmap (@->Effect (f a)) w
+    Eff m -> m >>= go . Effect
+    Failed _ v -> absurd v
+Effect m @-> rec = Effect $ fmap (@->rec) m
 
 (>-$) :: (Comonad w, Functor m) => Tape w (Drive e a m) b -> Drive e b m r -> Drive e a m r
 _ >-$ Done _ r = Done [] r
